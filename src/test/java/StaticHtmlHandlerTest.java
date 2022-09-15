db69691d7f64d529dfc67d65618040c9f4fcd605
taki.ng <taki.ng@kakaocorp.com>
2022-09-15 15:55:16 +0900

    chore: 메서드 수정된 이름 반영

--- src/test/java/StaticHtmlHandlerTest.java
+++ src/test/java/StaticHtmlHandlerTest.java
@@ -1,29 +1,29 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import urlHandler.handler.StaticHtmlHandler;
+import requestHandler.controller.StaticHtmlController;
+import webserver.ProcessedRequest;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class StaticHtmlHandlerTest {
 
-    StaticHtmlHandler staticHtmlHandler;
+    StaticHtmlController staticHtmlHandler;
 
     @BeforeEach
     void beforeEach() {
-        staticHtmlHandler = new StaticHtmlHandler();
+        staticHtmlHandler = new StaticHtmlController();
     }
 
     @Test
     @DisplayName("/index.html 접속 시 webapp/index.html 파일을 반환해야 한다")
     void test() throws IOException {
-        //given
-        String url = "/index.html";
 
         //when
-        byte[] sut = staticHtmlHandler.handle(url);
+        byte[] sut = staticHtmlHandler.process(new ProcessedRequest("", "/index.html", new HashMap<>()));
 
         //then
         assertThat(sut).isNotEmpty();
