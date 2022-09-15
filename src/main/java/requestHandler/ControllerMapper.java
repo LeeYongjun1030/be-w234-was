8bc89d0c94bc5026f37242b8dfccf556ceaee8fa
taki.ng <taki.ng@kakaocorp.com>
2022-09-15 15:51:00 +0900

    chore: ControllerMapper -> 클래스 이름 및 메서드 이름 변경

---
+++ src/main/java/requestHandler/ControllerMapper.java
@@ -0,0 +1,28 @@
+package requestHandler;
+
+import requestHandler.controller.StaticHtmlController;
+import requestHandler.controller.SignUpController;
+import requestHandler.controller.Controller;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerMapper {
+
+    private Map<String, Controller> handlers = new HashMap<>();
+
+    public ControllerMapper() {
+        // init
+        handlers.put("/user/create", new SignUpController());
+    }
+
+    public Controller controllerMapping(String path) {
+        File file = new File("./webapp" + path);
+        if (file.isFile()) { //정적 파일
+            return new StaticHtmlController();
+        }
+
+        return handlers.get(path);
+    }
+}
