6ad61cd635266f5ecec974fe68decf338cb89867
taki.ng <taki.ng@kakaocorp.com>
2022-09-19 18:15:32 +0900

    feat: 반환타입 변경(byte[] -> HttpResponse)로 인한 메서드 수정

--- src/main/java/controller/StaticHtmlController.java
+++ src/main/java/controller/StaticHtmlController.java
@@ -1,15 +1,43 @@
 package controller;
 
 import http.HttpRequest;
+import http.HttpResponse;
+import http.HttpStatus;
+import http.HttpStatusCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StaticHtmlController implements Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(StaticHtmlController.class);
+
     @Override
-    public byte[] process(HttpRequest httpRequest) throws IOException {
-        return Files.readAllBytes(new File("./webapp" + httpRequest.getPath()).toPath());
+    public HttpResponse process(HttpRequest httpRequest) {
+        try {
+            return getHttpResponse(httpRequest);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+
+    private HttpResponse getHttpResponse(HttpRequest httpRequest) throws IOException {
+        HttpStatus httpStatus = new HttpStatus(HttpStatusCode.SUCCESSFUL, "OK");
+        byte[] body = Files.readAllBytes(new File("./webapp" + httpRequest.getPath()).toPath());
+        Map<String, String> headers = createHeaders(body);
+        return new HttpResponse(httpStatus, headers, body);
+    }
+
+    private Map<String, String> createHeaders(byte[] body) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/html;charset=utf-8");
+        headers.put("Content-Length", String.valueOf(body.length));
+        return headers;
     }
 }
