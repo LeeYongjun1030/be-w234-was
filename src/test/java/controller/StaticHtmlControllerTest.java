f9ef0918ad62103d45f19a114d0a811fea1ea396
taki.ng <taki.ng@kakaocorp.com>
2022-09-19 18:09:17 +0900

    test: 반환타입 변경에 의한 테스트 수정

--- src/test/java/controller/StaticHtmlControllerTest.java
+++ src/test/java/controller/StaticHtmlControllerTest.java
@@ -1,35 +1,36 @@
 package controller;
 
 import http.HttpMethod;
+import http.HttpResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import controller.StaticHtmlController;
 import http.HttpRequest;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class StaticHtmlHandlerTest {
+public class StaticHtmlControllerTest {
 
-    StaticHtmlController staticHtmlHandler;
+    StaticHtmlController staticHtmlController;
 
     @BeforeEach
     void beforeEach() {
-        staticHtmlHandler = new StaticHtmlController();
+        staticHtmlController = new StaticHtmlController();
     }
 
     @Test
     @DisplayName("/index.html 접속 시 webapp/index.html 파일을 반환해야 한다")
-    void test() throws IOException {
+    void test() {
         //given
         HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "/index.html", null, null);
 
         //when
-        byte[] sut = staticHtmlHandler.process(httpRequest);
+        HttpResponse sut = staticHtmlController.process(httpRequest);
 
         //then
-        assertThat(sut).isNotEmpty();
+        assertThat(sut.getHttpStatus().getCode()).isEqualTo(200);
+        assertThat(sut.getBody()).isNotEmpty();
     }
 }
