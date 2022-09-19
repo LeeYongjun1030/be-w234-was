fd615895c146d622eca10cac4456e6938e092b23
taki.ng <taki.ng@kakaocorp.com>
2022-09-20 01:08:10 +0900

    feat: css파일 처리 코드 추가

--- src/main/java/controller/ControllerMapper.java
+++ src/main/java/controller/ControllerMapper.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ControllerMapper {
 
@@ -14,6 +15,9 @@ public class ControllerMapper {
     }
 
     public Controller controllerMapping(String path) {
+        if (Objects.equals(path, "/css/style.css")) {
+            return new StyleSheetController();
+        }
         File file = new File("./webapp" + path);
         if (file.isFile()) { //정적 파일
             return new StaticHtmlController();
