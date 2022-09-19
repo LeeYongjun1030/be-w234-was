e8b75957cfc131266395ca72a7227d2648a9a7b4
taki.ng <taki.ng@kakaocorp.com>
2022-09-19 18:15:25 +0900

    feat: 반환타입 변경(byte[] -> HttpResponse)로 인한 메서드 수정

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -1,17 +1,25 @@
 package controller;
 
 
+import http.HttpResponse;
+import http.HttpStatus;
+import http.HttpStatusCode;
 import model.User;
 import http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SignUpController implements Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(SignUpController.class);
+
     @Override
-    public byte[] process(HttpRequest httpRequest) {
+    public HttpResponse process(HttpRequest httpRequest) {
         User user = createUser(httpRequest.getParams());
-        return user.toString().getBytes();
+        return getHttpResponse(user);
     }
 
     private User createUser(Map<String, String> params) {
@@ -20,5 +28,18 @@ public class SignUpController implements Controller {
         String name = params.get("name");
         String email = params.get("email");
         return new User(userId, password, name, email);
+    }
+
+    private HttpResponse getHttpResponse(User user) {
+        HttpStatus httpStatus = new HttpStatus(HttpStatusCode.SUCCESSFUL, "OK");
+        Map<String, String> headers = createHeaders(user.toString().getBytes());
+        return new HttpResponse(httpStatus, headers, user.toString().getBytes());
+    }
+
+    private Map<String, String> createHeaders(byte[] body) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/html;charset=utf-8");
+        headers.put("Content-Length", String.valueOf(body.length));
+        return headers;
     }
 }
