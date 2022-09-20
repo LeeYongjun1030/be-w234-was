1649e4374c3064e935a1f9069e606c66cbc2d4f1
taki.ng <taki.ng@kakaocorp.com>
2022-09-20 23:27:56 +0900

    test: 데이터베이스에서 조회하는 방식으로 테스트를 변경

--- src/test/java/controller/SignUpControllerTest.java
+++ src/test/java/controller/SignUpControllerTest.java
@@ -1,8 +1,8 @@
 package controller;
 
-import http.HttpResponse;
-import http.HttpStatusCode;
+import db.Database;
 import model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,20 +18,25 @@ public class SignUpControllerTest {
        signUpController = new SignUpController();
     }
 
+    @AfterEach
+    void afterEach() {
+        Database.clear();
+    }
+
     @Test
     @DisplayName("회원가입 시 회원 정보가 User 클래스에 저장되어야 한다")
     void signup() {
         //given
-        String body = getBody("testId", "password", "taki", "taki@abcd.com");
+        String body = getBody("testId", "password", "타키", "taki@abcd.com");
         HttpRequest httpRequest = new HttpRequest(null, null, null, null, body);
+        signUpController.process(httpRequest);
 
         //when
-        HttpResponse sut = signUpController.process(httpRequest);
+        User sut = Database.findUserById("testId");
 
         //then
-        User user = new User("testId", "password", "taki", "taki@abcd.com");
-        assertThat(sut.getHttpStatus().getCode()).isEqualTo(HttpStatusCode.REDIRECT);
-        assertThat(sut.getBody()).isEqualTo(user.toString().getBytes());
+        assertThat(sut.getName()).isEqualTo("타키");
+        assertThat(sut.getEmail()).isEqualTo("taki@abcd.com");
     }
 
     private String getBody(String userId, String password, String name, String email) {
