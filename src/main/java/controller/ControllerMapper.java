920f42b3bebf82184b1555832cf6ad292a3b4f51
taki.ng <taki.ng@kakaocorp.com>
2022-09-20 12:15:33 +0900

    feat: css 처리 코드 작성

--- src/main/java/controller/ControllerMapper.java
+++ src/main/java/controller/ControllerMapper.java
@@ -3,7 +3,6 @@ package controller;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class ControllerMapper {
 
@@ -15,11 +14,12 @@ public class ControllerMapper {
     }
 
     public Controller controllerMapping(String path) {
-        if (Objects.equals(path, "/css/style.css")) {
-            return new StyleSheetController();
-        }
         File file = new File("./webapp" + path);
-        if (file.isFile()) { //정적 파일
+        if (file.isFile()) {
+            String[] s = path.split("\\.");
+            if (s[s.length - 1].equals("css")) {
+                return new StyleSheetController();
+            }
             return new StaticHtmlController();
         }
 
