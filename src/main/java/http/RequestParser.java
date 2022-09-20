3bf692bf54c89a75cdc44365d841e05eca24c89b
taki.ng <taki.ng@kakaocorp.com>
2022-09-20 15:28:42 +0900

    chore: 메서드 리팩토링(맥락 파악이 되도록)

--- src/main/java/http/RequestParser.java
+++ src/main/java/http/RequestParser.java
@@ -3,7 +3,6 @@ package http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.IOUtils;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -13,48 +12,34 @@ public class RequestParser {
     private static final Logger logger = LoggerFactory.getLogger(RequestParser.class);
 
     public HttpRequest parse(BufferedReader reader) throws IOException {
-
-        //start-line
         String startLine = reader.readLine();
-        HttpMethod httpMethod = extractHttpMethod(startLine);
-        String url = extractUrl(startLine);
-        String path = extractPath(url);
-        Map<String, String> params = extractParams(url);
+        String url = getUrl(startLine);
 
-        //headers
-        Map<String, String> headers = new HashMap<>();
-        String line;
-        while (!"".equals((line = reader.readLine()))&& line != null) {
-            String[] keyVal = line.split(":", 2);
-            headers.put(keyVal[0], keyVal[1]);
-        }
-
-        //body
-        String body = null;
-        if (headers.containsKey("Content-Length")) {
-            int contentLength = Integer.parseInt(headers.get("Content-Length").trim());
-            body = IOUtils.readData(reader, contentLength);
-        }
-
+        // make http request
+        HttpMethod httpMethod = getHttpMethod(startLine);
+        String path = getPath(url);
+        Map<String, String> params = getParams(url);
+        Map<String, String> headers = getHeaders(reader);
+        String body = getBody(reader, headers);
         return new HttpRequest(httpMethod, path, params, headers, body);
     }
 
-    private HttpMethod extractHttpMethod(String startLine) {
-        String[] tokens = startLine.split(" ");
-        return HttpMethod.valueOf(tokens[0].toUpperCase());
-    }
-
-    private String extractUrl(String startLine) {
+    private String getUrl(String startLine) {
         String[] tokens = startLine.split(" ");
         return tokens[1];
     }
 
-    private String extractPath(String url) {
+    private HttpMethod getHttpMethod(String startLine) {
+        String[] tokens = startLine.split(" ");
+        return HttpMethod.valueOf(tokens[0].toUpperCase());
+    }
+
+    private String getPath(String url) {
         String[] tokens = url.split("\\?");
         return tokens[0];
     }
 
-    private Map<String, String> extractParams(String url) {
+    private Map<String, String> getParams(String url) {
         Map<String, String> params = new HashMap<>();
 
         String[] tokens = url.split("\\?");
@@ -73,5 +58,24 @@ public class RequestParser {
             params.put(key, val);
         }
         return params;
+    }
+
+    private Map<String, String> getHeaders(BufferedReader reader) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String line;
+        while (!"".equals((line = reader.readLine()))&& line != null) {
+            String[] keyVal = line.split(":", 2);
+            headers.put(keyVal[0], keyVal[1]);
+        }
+        return headers;
+    }
+
+    private String getBody(BufferedReader reader, Map<String, String> headers) throws IOException {
+        String body = null;
+        if (headers.containsKey("Content-Length")) {
+            int contentLength = Integer.parseInt(headers.get("Content-Length").trim());
+            body = IOUtils.readData(reader, contentLength);
+        }
+        return body;
     }
 }
