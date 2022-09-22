30f4383a2229682e4cf9eb10617ca158bb69912f
taki.ng <taki.ng@kakaocorp.com>
2022-09-22 22:43:16 +0900

    feat: byte[]를 리턴하도록 메서드 수정

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -2,7 +2,6 @@ package http;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
 import java.util.Map;
 
 public class HttpResponse {
@@ -39,26 +38,38 @@ public class HttpResponse {
         return body;
     }
 
-    public ArrayList<byte[]> convertToHttpResponseMessage(){
-        ArrayList<byte[]> httpMessage = new ArrayList<>();
-        addStartLine(httpMessage);
-        addHeaders(httpMessage);
-        addBody(httpMessage);
-        return httpMessage;
+    public byte[] convertToHttpResponseMessage(){
+
+        byte[] startLine = createStartLine();
+        byte[] headers = createHeaders();
+
+        int totalLengthOfMsg = startLine.length + headers.length;
+        totalLengthOfMsg += (body != null) ? body.length : 0;
+
+        byte[] httpRespMessage = new byte[totalLengthOfMsg];
+        System.arraycopy(startLine, 0, httpRespMessage, 0, startLine.length);
+        System.arraycopy(headers, 0, httpRespMessage, startLine.length, headers.length);
+        if (body != null) {
+            System.arraycopy(body, 0, httpRespMessage, startLine.length + headers.length, body.length);
+        }
+        return httpRespMessage;
     }
 
-    private void addStartLine(ArrayList<byte[]> httpMessage) {
-        String startLine = String.format("HTTP/1.1 %s %s\r\n", httpStatus.getCode(), httpStatus.getReasonPhrase());
-        httpMessage.add(startLine.getBytes());
+    private byte[] createStartLine() {
+        return new StringBuilder()
+                .append("HTTP/1.1 ")
+                .append(httpStatus.getCode())
+                .append(" ")
+                .append(httpStatus.getReasonPhrase())
+                .append("\r\n")
+                .toString().getBytes();
     }
 
-    private void addHeaders(ArrayList<byte[]> httpMessage) {
-        headers.forEach((k, v) -> httpMessage.add(String.format("%s: %s\r\n", k, v).getBytes())); //headers
-        httpMessage.add("\r\n".getBytes()); //empty line
-    }
-
-    private void addBody(ArrayList<byte[]> httpMessage) {
-        httpMessage.add(body);
+    private byte[] createHeaders() {
+        StringBuilder sb = new StringBuilder();
+        headers.forEach((k, v) -> sb.append(k).append(": ").append(v).append("\r\n"));
+        sb.append("\r\n");
+        return sb.toString().getBytes();
     }
 
 }
