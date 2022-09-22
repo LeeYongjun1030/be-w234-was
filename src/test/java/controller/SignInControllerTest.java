40f6d0e4065ed54dd343fb5f889d1a0614604bf2
taki.ng <taki.ng@kakaocorp.com>
2022-09-22 17:55:25 +0900

    test: user를 지역 변수로 변경하여 재활용하는 방식으로 리팩토링

--- src/test/java/controller/SignInControllerTest.java
+++ src/test/java/controller/SignInControllerTest.java
@@ -8,15 +8,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SignInControllerTest {
 
-    Controller controller;
+    private Controller controller;
+    private User user = new User("testId", "1234", "taki", "taki@abcd.com");
 
     @BeforeEach
     void beforeEach() {
         controller = new SignInController();
+        Database.addUser(user);
     }
 
     @AfterEach
@@ -27,11 +30,8 @@ public class SignInControllerTest {
     @Test
     @DisplayName("로그인 입력 정보가 올바르면 로그인에 성공해야 한다")
     void signInSuccess() {
-        //given
-        createAndSaveUser();
-
         //when
-        HttpRequest loginInput = createHttpRequestFromInput("testId", "1234");
+        HttpRequest loginInput = createHttpRequestFromInput(user.getUserId(), user.getPassword());
         HttpResponse sut = controller.process(loginInput);
 
         //then
@@ -41,11 +41,8 @@ public class SignInControllerTest {
     @Test
     @DisplayName("존재하지 않는 로그인 아이디를 입력하면 로그인에 실패해야 한다")
     void signInFail() {
-        //given
-        createAndSaveUser();
-
         //when
-        HttpRequest loginInput = createHttpRequestFromInput("xxxx", "1234");
+        HttpRequest loginInput = createHttpRequestFromInput(user.getUserId() + "-make-wrong", user.getPassword());
         HttpResponse sut = controller.process(loginInput);
 
         //then
@@ -55,20 +52,12 @@ public class SignInControllerTest {
     @Test
     @DisplayName("아이디는 올바르나 비밀번호가 올바르지 않으면 로그인에 실패해야 한다")
     void signInFail2() {
-        //given
-        createAndSaveUser();
-
         //when
-        HttpRequest loginInput = createHttpRequestFromInput("testId", "4321");
+        HttpRequest loginInput = createHttpRequestFromInput(user.getUserId(), user.getPassword() + "-make-wrong");
         HttpResponse sut = controller.process(loginInput);
 
         //then
         assertThat(sut.getHeaders().get("Set-Cookie")).isEqualTo("logined=false; Path=/");
-    }
-
-    private void createAndSaveUser() {
-        User user = new User("testId", "1234", "taki", "taki@abcd.com");
-        Database.addUser(user);
     }
 
     private HttpRequest createHttpRequestFromInput(String userId, String password) {
