168489a4f01b0000a2dde9abc2fa8b891b609cf6
taki.ng <taki.ng@kakaocorp.com>
2022-09-23 15:45:15 +0900

    test: Http 응답 메시지 생성에 대한 테스트 작성. body가 있는 경우와 없는 경우 둘 다 테스트

--- src/test/java/http/HttpResponseTest.java
+++ src/test/java/http/HttpResponseTest.java
@@ -2,38 +2,50 @@ package http;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpResponseTest {
 
+    HttpResponse httpResponse;
+
     @Test
-    @DisplayName("HttpResponse 객체로부터 http 응답 메시지를 만들어낼 수 있어야 한다")
-    void parsingTest() {
+    @DisplayName("HttpResponse 객체로부터 http 응답 메시지를 만들어낼 수 있어야 한다 - body가 없는 경우")
+    void createMsgWithBodilessResponse() {
         //given
-        HttpResponse httpResponse = getHttpResponse();
+        httpResponse = new HttpResponse.Builder(HttpStatus.SUCCESSFUL)
+                .header("Content-Type", "text/html;charset=utf-8")
+                .build();
 
         //when
-        ArrayList<byte[]> sut = httpResponse.convertToHttpResponseMessage();
+        byte[] sut = httpResponse.convertToHttpResponseMessage();
 
         //then
-        assertThat(sut.get(0)).isEqualTo("HTTP/1.1 200 OK\r\n".getBytes());
-        assertThat(sut.get(1)).isEqualTo("Content-Length: 22\r\n".getBytes());
-        assertThat(sut.get(2)).isEqualTo("Content-Type: text/html;charset=utf-8\r\n".getBytes());
-        assertThat(sut.get(3)).isEqualTo("\r\n".getBytes());
-        assertThat(sut.get(4)).isEqualTo("This is response body.".getBytes());
+        String[] token = new String(sut).split("\r\n");
+        assertThat(token[0]).isEqualTo("HTTP/1.1 200 OK");
+        assertThat(token[1]).isEqualTo("Content-Type: text/html;charset=utf-8");
     }
 
-    private HttpResponse getHttpResponse() {
-        HttpStatus httpStatus = new HttpStatus(HttpStatusCode.SUCCESSFUL, "OK");
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "text/html;charset=utf-8");
-        String bodyOfString = "This is response body.";
-        byte[] body = bodyOfString.getBytes();
-        headers.put("Content-Length", String.valueOf(body.length));
-        return new HttpResponse(httpStatus, headers, body);
+    @Test
+    @DisplayName("HttpResponse 객체로부터 http 응답 메시지를 만들어낼 수 있어야 한다 - body가 있는 경우")
+    void createMsgWithResponse() {
+        //given
+        String dataOfString = "This is response body.";
+        byte[] data = dataOfString.getBytes();
+
+        httpResponse = new HttpResponse.Builder(HttpStatus.SUCCESSFUL)
+                .header("Content-Type", "text/html;charset=utf-8")
+                .header("Content-Length", String.valueOf(data.length))
+                .body(data)
+                .build();
+
+        //when
+        byte[] sut = httpResponse.convertToHttpResponseMessage();
+
+        //then
+        String[] token = new String(sut).split("\r\n");
+        assertThat(token[0]).isEqualTo("HTTP/1.1 200 OK");
+        assertThat(token[1]).isEqualTo("Content-Length: " + httpResponse.getBody().length);
+        assertThat(token[2]).isEqualTo("Content-Type: text/html;charset=utf-8");
+
     }
 }
