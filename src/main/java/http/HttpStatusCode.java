fb72f6a7b02a78e8c579d463e3e6fa00d3d18cc2
taki.ng <taki.ng@kakaocorp.com>
2022-09-23 13:50:18 +0900

    chore: 미사용 클래스 삭제

--- src/main/java/http/HttpStatusCode.java
+++
@@ -1,8 +0,0 @@
-package http;
-
-public class HttpStatusCode {
-    public static final int SUCCESSFUL = 200;
-    public static final int REDIRECT= 302;
-    public static final int CLIENT_ERROR = 400;
-    public static final int SERVER_ERROR = 500;
-}
