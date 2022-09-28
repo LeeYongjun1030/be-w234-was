eac16370755ba1248ce0616e0feab6889a8b4c4f
taki.ng <taki.ng@kakaocorp.com>
2022-09-28 14:25:13 +0900

    refactor: 빌더 패턴 수정으로 인한 리팩토링

--- src/test/java/http/RequestParserTest.java
+++ src/test/java/http/RequestParserTest.java
@@ -5,17 +5,12 @@ import org.junit.jupiter.api.Test;
 import project.http.HttpMethod;
 import project.http.HttpRequest;
 import project.http.RequestParser;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class RequestParserTest {
-
-    RequestParser requestParser = new RequestParser();
-    BufferedReader br;
 
     @Test
     @DisplayName("http 요청 메시지를 파싱하여 HttpRequest 객체를 만들어낼 수 있어야 한다.")
@@ -23,10 +18,10 @@ public class RequestParserTest {
         //given
         String msg = "GET /test?name=james&age=28 HTTP/1.1\n" +
                 "Connection: keep-alive";
-        br = new BufferedReader(new StringReader(msg));
+        RequestParser requestParser = createRequestParser(msg);
 
         //when
-        HttpRequest sut = requestParser.parse(br);
+        HttpRequest sut = requestParser.parse();
 
         //then
         assertThat(sut.getHttpMethod()).isEqualTo(HttpMethod.GET);
@@ -36,16 +31,21 @@ public class RequestParserTest {
     }
 
     @Test
-    @DisplayName("url에 파라미터가 없는 경우, params는 null을 반환해야 한다")
+    @DisplayName("url에 파라미터가 없는 경우, 생성된 HttpRequest의 params 속성은 빈 객체여야 한다")
     void getParamsNullTest() throws IOException {
         //given
         String msg = "GET /test HTTP/1.1";
-        br = new BufferedReader(new StringReader(msg));
+        RequestParser requestParser = createRequestParser(msg);
 
         //when
-        HttpRequest sut = requestParser.parse(br);
+        HttpRequest sut = requestParser.parse();
 
         //then
-        assertThat(sut.getParams()).isNull();
+        assertThat(sut.getParams().size()).isEqualTo(0);
+    }
+
+    private RequestParser createRequestParser(String msg) {
+        BufferedReader br = new BufferedReader(new StringReader(msg));
+        return new RequestParser(br);
     }
 }
